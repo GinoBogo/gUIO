cc019ec53dca4d079a2d66763f3147f98a9d901c
Gino Bogo <gino.bogo@gmail.com>
2021-12-20 09:11:18 +0100

    Added the "GProfile" class.

---
+++ src/GProfile.hpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file      GProfile.hpp
+/// \version   0.1
+/// \date      December, 2021
+/// \author    Gino Francesco Bogo
+/// \copyright This file is released under the MIT license
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GPROFILE_HPP_
+#define GPROFILE_HPP_
+
+#include <chrono>
+
+class GProfile {
+    public:
+    void Start() {
+        m_t0 = std::chrono::system_clock::now();
+    }
+
+    void Stop() {
+        m_t1 = std::chrono::system_clock::now();
+    }
+
+    auto ms() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(m_t1 - m_t0).count();
+    }
+
+    auto us() {
+        return std::chrono::duration_cast<std::chrono::microseconds>(m_t1 - m_t0).count();
+    }
+
+    auto ns() {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(m_t1 - m_t0).count();
+    }
+
+    private:
+    std::chrono::time_point<std::chrono::system_clock> m_t0;
+    std::chrono::time_point<std::chrono::system_clock> m_t1;
+};
+
+#endif /* GPROFILE_HPP_ */
