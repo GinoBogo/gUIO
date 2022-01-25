b0e5e183206bbb43b690f2693239153baa2f29bf
Gino Bogo <gino.bogo@gmail.com>
2022-01-25 09:52:12 +0100

    Flows update in the "main" file.

--- examples/MUDP/src/main.cpp
+++ examples/MUDP/src/main.cpp
@@ -13,10 +13,15 @@
 
 #include <thread>
 
-#define CTRLX_SERVER_ADDR "127.0.0.1"
-#define CTRLX_SERVER_PORT 40001
-#define CTRLX_CLIENT_ADDR "127.0.0.1"
-#define CTRLX_CLIENT_PORT 40101
+#define GM_MC_SERVER_ADDR "127.0.0.1"
+#define GM_MC_SERVER_PORT 30001
+#define GM_MC_CLIENT_ADDR "127.0.0.1"
+#define GM_MC_CLIENT_PORT 30101
+
+#define GM_DH_SERVER_ADDR "127.0.0.1"
+#define GM_DH_SERVER_PORT 40001
+#define GM_DH_CLIENT_ADDR "127.0.0.1"
+#define GM_DH_CLIENT_PORT 40101
 
 #define HSSL0_SERVER_ADDR "127.0.0.1"
 #define HSSL0_SERVER_PORT 50001
@@ -28,8 +33,8 @@
 #define HSSL1_CLIENT_ADDR "127.0.0.1"
 #define HSSL1_CLIENT_PORT 60101
 
-void f_ctrlx_server(GUdpServer &p_server, GUdpClient &p_client) {
-    LOG_WRITE(trace, "Thread STARTED (ctrlx_server)");
+void f_gm_mc_server(GUdpServer &p_server, GUdpClient &p_client) {
+    LOG_WRITE(trace, "Thread STARTED (gm_mc_server)");
 
     uint8_t buffer[GUdpServer::MAX_DATAGRAM_SIZE];
     size_t  bytes;
@@ -76,7 +81,13 @@ void f_ctrlx_server(GUdpServer &p_server, GUdpClient &p_client) {
         }
     }
 
-    LOG_WRITE(trace, "Thread STOPPED (ctrlx_server)");
+    LOG_WRITE(trace, "Thread STOPPED (gm_mc_server)");
+}
+
+void f_gm_dh_server(GUdpServer &p_server, GUdpClient &p_client) {
+    LOG_WRITE(trace, "Thread STARTED (gm_dh_server)");
+
+    LOG_WRITE(trace, "Thread STOPPED (gm_dh_server)");
 }
 
 void f_hssl0_server(GUdpServer &p_server, GUdpClient &p_client) {
@@ -95,18 +106,22 @@ int main() {
     GLogger::Initialize("example_MUDP.log");
     LOG_WRITE(trace, "Process STARTED (main)");
 
-    auto ctrlx_server = GUdpServer(CTRLX_SERVER_ADDR, CTRLX_SERVER_PORT, "CTRL x");
-    auto ctrlx_client = GUdpClient(CTRLX_CLIENT_ADDR, CTRLX_CLIENT_PORT, "CTRL x");
-    auto hssl0_server = GUdpServer(HSSL0_SERVER_ADDR, HSSL0_SERVER_PORT, "HSSL 0");
-    auto hssl0_client = GUdpClient(HSSL0_CLIENT_ADDR, HSSL0_CLIENT_PORT, "HSSL 0");
-    auto hssl1_server = GUdpServer(HSSL1_SERVER_ADDR, HSSL1_SERVER_PORT, "HSSL 1");
-    auto hssl1_client = GUdpClient(HSSL1_CLIENT_ADDR, HSSL1_CLIENT_PORT, "HSSL 1");
+    auto gm_mc_server = GUdpServer(GM_MC_SERVER_ADDR, GM_MC_SERVER_PORT, "GM-MC");
+    auto gm_mc_client = GUdpClient(GM_MC_CLIENT_ADDR, GM_MC_CLIENT_PORT, "GM-MC");
+    auto gm_dh_server = GUdpServer(GM_DH_SERVER_ADDR, GM_DH_SERVER_PORT, "GM-DH");
+    auto gm_dh_client = GUdpClient(GM_DH_CLIENT_ADDR, GM_DH_CLIENT_PORT, "GM-DH");
+    auto hssl0_server = GUdpServer(HSSL0_SERVER_ADDR, HSSL0_SERVER_PORT, "HSSL0");
+    auto hssl0_client = GUdpClient(HSSL0_CLIENT_ADDR, HSSL0_CLIENT_PORT, "HSSL0");
+    auto hssl1_server = GUdpServer(HSSL1_SERVER_ADDR, HSSL1_SERVER_PORT, "HSSL1");
+    auto hssl1_client = GUdpClient(HSSL1_CLIENT_ADDR, HSSL1_CLIENT_PORT, "HSSL1");
 
-    std::thread t_ctrlx_server(f_ctrlx_server, std::ref(ctrlx_server), std::ref(ctrlx_client));
+    std::thread t_gm_mc_server(f_gm_mc_server, std::ref(gm_mc_server), std::ref(gm_mc_client));
+    std::thread t_gm_dh_server(f_gm_dh_server, std::ref(gm_dh_server), std::ref(gm_dh_client));
     std::thread t_hssl0_server(f_hssl0_server, std::ref(hssl0_server), std::ref(hssl0_client));
     std::thread t_hssl1_server(f_hssl1_server, std::ref(hssl1_server), std::ref(hssl1_client));
 
-    t_ctrlx_server.join();
+    t_gm_mc_server.join();
+    t_gm_dh_server.join();
     t_hssl0_server.join();
     t_hssl1_server.join();
 
