b49ef9763ccd84f0abd528851ea800f7259cb713
Gino Bogo <gino.bogo@gmail.com>
2022-02-02 11:55:23 +0100

    Added the "GEncoder" class.

---
+++ examples/MUDP/src/GEncoder.hpp
@@ -0,0 +1,99 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file      GEncoder.hpp
+/// \version   0.1
+/// \date      February, 2022
+/// \author    Gino Francesco Bogo
+/// \copyright This file is released under the MIT license
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef GENCODER_HPP
+#define GENCODER_HPP
+
+#include "GFiFo.hpp"
+#include "GMessage.hpp"
+
+#include <string.h> // bzero
+
+class GEncoder {
+    public:
+    static const auto fifo_depth = 20;
+
+    GEncoder(uint32_t file_id) {
+        m_packet_counter = 1;
+        m_file_id        = file_id;
+    }
+
+    void Reset() {
+        m_fifo.Reset();
+    }
+
+    bool Process(uint8_t packet_type, uint8_t *message_data = nullptr, uint32_t message_length = 0) {
+        bzero(&m_packet, GPacket::PACKET_HEAD_SIZE);
+
+        auto result{false};
+
+        m_packet.head.packet_type     = packet_type;
+        m_packet.head.packet_counter  = m_packet_counter++;
+        m_packet.head.current_segment = 1;
+
+        if (!message_length) {
+            m_packet.head.total_segments = 1;
+
+            result = m_fifo.Push((uint8_t *)&m_packet, GPacket::PACKET_HEAD_SIZE);
+        }
+        else {
+            if (message_data != nullptr) {
+                auto _num = (uint16_t)(message_length / GPacket::PACKET_DATA_SIZE);
+                auto _rem = (uint16_t)(message_length % GPacket::PACKET_DATA_SIZE);
+
+                m_packet.head.file_id        = m_file_id;
+                m_packet.head.total_segments = _num + (uint16_t)(_rem != 0);
+
+                auto _loop{true};
+
+                while (_loop && _num--) {
+                    m_packet.head.data_length = GPacket::PACKET_DATA_SIZE;
+                    memcpy(m_packet.data.bytes, message_data, GPacket::PACKET_DATA_SIZE);
+                    _loop = m_fifo.Push((uint8_t *)&m_packet, GPacket::PACKET_FULL_SIZE);
+
+                    message_data += GPacket::PACKET_DATA_SIZE;
+                    m_packet.head.current_segment += 1;
+                }
+
+                if (_loop && _rem != 0) {
+                    m_packet.head.data_length = _rem;
+                    memcpy(m_packet.data.bytes, message_data, _rem);
+                    _loop = m_fifo.Push((uint8_t *)&m_packet, GPacket::PACKET_HEAD_SIZE + _rem);
+                }
+
+                result = _loop;
+            }
+        }
+
+        if (!result) {
+            LOG_WRITE(error, "Unable to encode message");
+        }
+
+        return result;
+    }
+
+    auto IsEmpty() {
+        return m_fifo.IsEmpty();
+    }
+
+    auto IsFull() {
+        return m_fifo.IsFull();
+    }
+
+    auto Pop(uint8_t *dst_data, uint32_t dst_size) {
+        return m_fifo.Pop(dst_data, dst_size);
+    }
+
+    private:
+    uint32_t m_packet_counter;
+    uint32_t m_file_id;
+    TPacket  m_packet;
+    GFiFo    m_fifo = GFiFo(GPacket::PACKET_FULL_SIZE, fifo_depth);
+};
+
+#endif // GENCODER_HPP
