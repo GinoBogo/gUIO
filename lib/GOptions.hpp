096ac0b551b283a03b0ff8c74dfbe0a120318e4f
Gino Bogo <gino.bogo@gmail.com>
2022-03-03 13:27:45 +0100

    Added C++14 SFINAE mode.

--- lib/GOptions.hpp
+++ lib/GOptions.hpp
@@ -14,6 +14,8 @@
 #include <map>
 #include <string>
 
+//#define ENABLE_SFINAE
+
 class GOptions : public std::map<std::string, std::any> {
     public:
     struct Pair {
@@ -37,33 +39,18 @@ class GOptions : public std::map<std::string, std::any> {
 
     typedef std::list<Section> Sections;
 
-    template <typename T> void Insert(const std::string &label, const std::any &value);
-
-    template <typename T> T Get(const std::string &label);
-
-    Pairs ToPairs();
-
-    Sections ToSections();
-
-    bool Read(const std::string &filename);
-
-    bool Write(const std::string &filename);
-
-    GOptions &operator+=(const GOptions &options);
-};
-
-template <typename T> void GOptions::Insert(const std::string &label, const std::any &value) {
-    if (value.type() == typeid(T)) {
-        this->insert_or_assign(label, std::any_cast<T>(value));
-    }
-    else {
-        if constexpr (std::is_same_v<T, std::string>) {
-            if (value.type() == typeid(const char *)) {
-                this->insert_or_assign(label, std::string(std::any_cast<const char *>(value)));
-            }
+#ifndef ENABLE_SFINAE
+    template <typename T> void Insert(const std::string &label, const std::any &value) {
+        if (value.type() == typeid(T)) {
+            this->insert_or_assign(label, std::any_cast<T>(value));
         }
         else {
-            if (value.type() == typeid(bool)) {
+            if constexpr (std::is_same_v<T, std::string>) {
+                if (value.type() == typeid(const char *)) {
+                    this->insert_or_assign(label, std::string(std::any_cast<const char *>(value)));
+                }
+            }
+            else if (value.type() == typeid(bool)) {
                 this->insert_or_assign(label, static_cast<T>(std::any_cast<bool>(value)));
             }
             else if (value.type() == typeid(char)) {
@@ -107,19 +94,17 @@ template <typename T> void GOptions::Insert(const std::string &label, const std:
             }
         }
     }
-}
 
-template <typename T> T GOptions::Get(const std::string &label) {
-    T type{};
+    template <typename T> T Get(const std::string &label) {
+        T type{};
 
-    auto found = this->find(label);
-    if (found != this->end()) {
-        const auto &value{found->second};
-        if (value.type() == typeid(T)) {
-            type = std::any_cast<T>(value);
-        }
-        else {
-            if constexpr (!std::is_same_v<T, std::string>) {
+        auto found = this->find(label);
+        if (found != this->end()) {
+            const auto &value{found->second};
+            if (value.type() == typeid(T)) {
+                type = std::any_cast<T>(value);
+            }
+            else if constexpr (!std::is_same_v<T, std::string>) {
                 if (value.type() == typeid(bool)) {
                     type = static_cast<T>(std::any_cast<bool>(value));
                 }
@@ -164,8 +149,154 @@ template <typename T> T GOptions::Get(const std::string &label) {
                 }
             }
         }
+
+        return type;
     }
-    return type;
-}
+#else
+    template <typename A, typename B> using accept_if_string = std::enable_if_t<std::is_same_v<A, std::string>, B>;
+
+    template <typename A, typename B> using accept_if_number = std::enable_if_t<!std::is_same_v<A, std::string>, B>;
+
+    template <typename T> void Insert(const std::string &label, const std::any &value, accept_if_string<T, bool> _ = 0) {
+        if (value.type() == typeid(T)) {
+            this->insert_or_assign(label, std::any_cast<T>(value));
+        }
+        else if (value.type() == typeid(const char *)) {
+            this->insert_or_assign(label, std::string(std::any_cast<const char *>(value)));
+        }
+    }
+
+    template <typename T> void Insert(const std::string &label, const std::any &value, accept_if_number<T, long> _ = 0) {
+        if (value.type() == typeid(T)) {
+            this->insert_or_assign(label, std::any_cast<T>(value));
+        }
+        else if (value.type() == typeid(bool)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<bool>(value)));
+        }
+        else if (value.type() == typeid(char)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<char>(value)));
+        }
+        else if (value.type() == typeid(unsigned char)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<unsigned char>(value)));
+        }
+        else if (value.type() == typeid(short)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<short>(value)));
+        }
+        else if (value.type() == typeid(unsigned short)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<unsigned short>(value)));
+        }
+        else if (value.type() == typeid(int)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<int>(value)));
+        }
+        else if (value.type() == typeid(unsigned int)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<unsigned int>(value)));
+        }
+        else if (value.type() == typeid(long)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<long>(value)));
+        }
+        else if (value.type() == typeid(unsigned long)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<unsigned long>(value)));
+        }
+        else if (value.type() == typeid(long long)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<long long>(value)));
+        }
+        else if (value.type() == typeid(unsigned long long)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<unsigned long long>(value)));
+        }
+        else if (value.type() == typeid(float)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<float>(value)));
+        }
+        else if (value.type() == typeid(double)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<double>(value)));
+        }
+        else if (value.type() == typeid(long double)) {
+            this->insert_or_assign(label, static_cast<T>(std::any_cast<long double>(value)));
+        }
+    }
+
+    template <typename T> T Get(const std::string &label, accept_if_string<T, bool> _ = 0) {
+        T type{};
+
+        auto found = this->find(label);
+        if (found != this->end()) {
+            const auto &value{found->second};
+            if (value.type() == typeid(T)) {
+                type = std::any_cast<T>(value);
+            }
+            else if (value.type() == typeid(const char *)) {
+                type = std::string(std::any_cast<const char *>(value));
+            }
+        }
+
+        return type;
+    }
+
+    template <typename T> T Get(const std::string &label, accept_if_number<T, long> _ = 0) {
+        T type{};
+
+        auto found = this->find(label);
+        if (found != this->end()) {
+            const auto &value{found->second};
+            if (value.type() == typeid(T)) {
+                type = std::any_cast<T>(value);
+            }
+            else if (value.type() == typeid(bool)) {
+                type = static_cast<T>(std::any_cast<bool>(value));
+            }
+            else if (value.type() == typeid(char)) {
+                type = static_cast<T>(std::any_cast<char>(value));
+            }
+            else if (value.type() == typeid(unsigned char)) {
+                type = static_cast<T>(std::any_cast<unsigned char>(value));
+            }
+            else if (value.type() == typeid(short)) {
+                type = static_cast<T>(std::any_cast<short>(value));
+            }
+            else if (value.type() == typeid(unsigned short)) {
+                type = static_cast<T>(std::any_cast<unsigned short>(value));
+            }
+            else if (value.type() == typeid(int)) {
+                type = static_cast<T>(std::any_cast<int>(value));
+            }
+            else if (value.type() == typeid(unsigned int)) {
+                type = static_cast<T>(std::any_cast<unsigned int>(value));
+            }
+            else if (value.type() == typeid(long)) {
+                type = static_cast<T>(std::any_cast<long>(value));
+            }
+            else if (value.type() == typeid(unsigned long)) {
+                type = static_cast<T>(std::any_cast<unsigned long>(value));
+            }
+            else if (value.type() == typeid(long long)) {
+                type = static_cast<T>(std::any_cast<long long>(value));
+            }
+            else if (value.type() == typeid(unsigned long long)) {
+                type = static_cast<T>(std::any_cast<unsigned long long>(value));
+            }
+            else if (value.type() == typeid(float)) {
+                type = static_cast<T>(std::any_cast<float>(value));
+            }
+            else if (value.type() == typeid(double)) {
+                type = static_cast<T>(std::any_cast<double>(value));
+            }
+            else if (value.type() == typeid(long double)) {
+                type = static_cast<T>(std::any_cast<long double>(value));
+            }
+        }
+
+        return type;
+    }
+#endif // ENABLE_SFINAE
+
+    Pairs ToPairs();
+
+    Sections ToSections();
+
+    bool Read(const std::string &filename);
+
+    bool Write(const std::string &filename);
+
+    GOptions &operator+=(const GOptions &options);
+};
 
 #endif // GOPTIONS_HPP
