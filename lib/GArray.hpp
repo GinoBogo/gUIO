d01386b3aab979477c6a2d0ea3ca184af7a22c03
Gino Bogo <gino.bogo@gmail.com>
2022-05-13 10:30:09 +0200

    Added copy constructor to the "GArray" class.

--- lib/GArray.hpp
+++ lib/GArray.hpp
@@ -10,28 +10,51 @@
 #define GARRAY_HPP
 
 #include <cstddef>     // size_t
-#include <type_traits> // is_pointer_v
+#include <type_traits> // is_fundamental_v
 
 template <typename T> class GArray {
     public:
-    GArray(size_t size) : m_size{size} {
-        m_data = new T[m_size];
+    GArray(size_t size) {
+        static_assert(std::is_fundamental_v<T>, "Type not supported.");
+
+        m_data = new T[size];
+        m_size = size;
         m_used = 0;
     }
 
-    ~GArray() {
-        if constexpr (std::is_pointer_v<T>) {
-            for (decltype(m_used) i{0}; i < m_size;) delete m_data[i++];
+    GArray(const GArray& array) {
+        if (this != &array) {
+            m_data = new T[array.m_size];
+            m_size = array.m_size;
+            m_used = array.m_used;
+
+            for (decltype(m_used) i{0}; i < m_used; ++i) m_data[i] = array.m_data[i];
         }
-        delete[] m_data;
     }
 
-    auto size() {
-        return m_size;
+    ~GArray() {
+        release_resources();
+    }
+
+    GArray& operator=(const GArray& array) {
+        if (this != &array) {
+            release_resources();
+
+            m_data = new T[array.m_size];
+            m_size = array.m_size;
+            m_used = array.m_used;
+
+            for (decltype(m_used) i{0}; i < m_used; ++i) m_data[i] = array.m_data[i];
+        }
+        return *this;
     }
 
     auto data() {
         return m_data;
+    }
+
+    auto size() {
+        return m_size;
     }
 
     auto used() {
@@ -55,9 +78,16 @@ template <typename T> class GArray {
     }
 
     private:
-    const size_t m_size;
-    T*           m_data;
-    size_t       m_used;
+    auto release_resources() {
+        if (m_data != nullptr) {
+            delete[] m_data;
+            m_data = nullptr;
+        }
+    }
+
+    T*     m_data{nullptr};
+    size_t m_size{0};
+    size_t m_used{0};
 };
 
 #endif // GARRAY_HPP
