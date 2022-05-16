bd563b1f72fc69a4694d3ad4514e595cc34cdf42
Gino Bogo <gino.bogo@gmail.com>
2022-05-16 09:37:00 +0200

    Major update of the "FIFO" example.

--- examples/FIFO/src/main.cpp
+++ examples/FIFO/src/main.cpp
@@ -7,6 +7,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "GArray.hpp"
+#include "GArrayRoller.hpp"
 #include "GFIFOdevice.hpp"
 #include "GLogger.hpp"
 #include "GProfile.hpp"
@@ -35,8 +36,8 @@
 #define SPACE_PACKET_BYTES 65536
 #define SPACE_PACKET_WORDS (SPACE_PACKET_BYTES / FIFO_WORD_SIZE)
 
-const auto ENABLE_TX_MODE = 0;
-const auto ENABLE_RX_MODE = 1;
+const auto ENABLE_TX_MODE = 1;
+const auto ENABLE_RX_MODE = 0;
 
 static auto load_packet_words(uint16_t* buffer, uint32_t words, const char* filename = nullptr) {
     auto _ret{false};
@@ -73,7 +74,7 @@ static auto load_packet_words(uint16_t* buffer, uint32_t words, const char* file
 int p_num = 0; // dump progressive number
 int p_val = 0; // dump previous byte value
 
-static auto dump_packet_words(uint16_t* buffer, uint32_t words, const char* filename = nullptr) {
+static auto dump_packet_words(const uint16_t* buffer, uint32_t words, const char* filename = nullptr) {
     auto _ret{false};
 
     if (buffer != nullptr && words > 0) {
@@ -82,11 +83,18 @@ static auto dump_packet_words(uint16_t* buffer, uint32_t words, const char* file
             fs.open(filename);
 
             if (fs.is_open()) {
-                char line[64];
+                char h_line[16];
+                char l_line[16];
 
                 for (decltype(words) i{0}; i < words; ++i) {
-                    sprintf(line, "0x%04X\n", buffer[i]);
-                    fs.write(line, (signed)strlen(line));
+                    auto h_val{(0xFF00 & buffer[i]) >> 8};
+                    auto l_val{(0x00FF & buffer[i]) >> 0};
+
+                    sprintf(h_line, "0x%02X\n", h_val);
+                    sprintf(l_line, "0x%02X\n", l_val);
+
+                    fs.write(h_line, (signed)strlen(h_line));
+                    fs.write(l_line, (signed)strlen(l_line));
                 }
 
                 fs.close();
@@ -97,6 +105,9 @@ static auto dump_packet_words(uint16_t* buffer, uint32_t words, const char* file
                 auto h_val{(0xFF00 & buffer[i]) >> 8};
                 auto l_val{(0x00FF & buffer[i]) >> 0};
 
+                auto h_dif{h_val - p_val};
+                auto l_dif{l_val - h_val};
+
                 auto check_1{(h_val == 0) && (l_val == 247)};
                 auto check_2{(p_val == 0) && (h_val == 247)};
 
@@ -106,8 +117,8 @@ static auto dump_packet_words(uint16_t* buffer, uint32_t words, const char* file
 
                 p_val = l_val;
 
-                LOG_FORMAT(debug, " (%4d) %3d", p_num++, h_val);
-                LOG_FORMAT(debug, " (%4d) %3d", p_num++, l_val);
+                LOG_FORMAT(debug, " %4d | %3d | %+4d", p_num++, h_val, h_dif);
+                LOG_FORMAT(debug, " %4d | %3d | %+4d", p_num++, l_val, l_dif);
             }
         }
     }
@@ -116,7 +127,7 @@ static auto dump_packet_words(uint16_t* buffer, uint32_t words, const char* file
 }
 
 int main(int argc, char* argv[]) {
-    GLogger::Initialize("MUST_FIFO.log");
+    GLogger::Initialize("_fifo.log");
     LOG_FORMAT(trace, "Process STARTED (%s)", __func__);
 
     if (ENABLE_TX_MODE) {
@@ -125,35 +136,35 @@ int main(int argc, char* argv[]) {
 
         if (tx_fifo.Open()) {
             if (argc > 1) {
-                load_packet_words(tx_buff.ptr, tx_buff.len, argv[1]);
+                load_packet_words(tx_buff.data(), tx_buff.size(), argv[1]);
             }
             else {
-                load_packet_words(tx_buff.ptr, tx_buff.len);
+                load_packet_words(tx_buff.data(), tx_buff.size());
             }
 
-            if (tx_fifo.SetTxPacketWords(tx_buff.len)) {
-                bool _err, _res{true};
+            if (tx_fifo.SetTxPacketWords(tx_buff.size())) {
+                auto _err   = false;
                 auto _loop  = 2000;
                 auto _bytes = 0UL;
 
-                LOG_FORMAT(debug, "[TX] Packet words %d", tx_fifo.GetTxPacketWords(&_err));
-                LOG_FORMAT(debug, "[TX] Unused words %d", tx_fifo.GetTxUnusedWords(&_err));
+                LOG_FORMAT(debug, "[TX] Packet words %d", tx_fifo.GetTxPacketWords(_err));
+                LOG_FORMAT(debug, "[TX] Unused words %d", tx_fifo.GetTxUnusedWords(_err));
 
                 tx_fifo.Reset();
                 tx_fifo.ClearEvent();
 
                 tx_fifo.EnableReader();
-                tx_fifo.WritePacket(tx_buff.ptr, tx_buff.len);
+                tx_fifo.WritePacket(tx_buff.data(), tx_buff.size());
 
                 GProfile profile;
                 profile.Start();
                 // -------------------------------------------------------------------
-                for (decltype(_loop) i{0}; (i < _loop) && _res; ++i) {
-                    _res = tx_fifo.WritePacket(tx_buff.ptr, tx_buff.len);
-                    if (_res) {
-                        _res = tx_fifo.WaitThenClearEvent();
-                        if (_res) {
-                            _bytes += FIFO_WORD_SIZE * tx_buff.len;
+                for (decltype(_loop) i{0}; (i < _loop) && !_err; ++i) {
+                    _err = !tx_fifo.WritePacket(tx_buff.data(), tx_buff.size());
+                    if (!_err) {
+                        _err = !tx_fifo.WaitThenClearEvent();
+                        if (!_err) {
+                            _bytes += FIFO_WORD_SIZE * tx_buff.size();
                         }
                     }
                 }
@@ -168,11 +179,11 @@ int main(int argc, char* argv[]) {
 
     if (ENABLE_RX_MODE) {
         auto rx_fifo = GFIFOdevice(RX_FIFO_DEV_ADDR, RX_FIFO_DEV_SIZE, RX_FIFO_UIO_NUM, RX_FIFO_UIO_MAP);
-        auto rx_buff = GArray<uint16_t>(SPACE_PACKET_WORDS);
+        auto rx_buff = GArrayRoller<uint16_t>(SPACE_PACKET_WORDS, 20);
 
         if (rx_fifo.Open()) {
-            bool _err, _res{true};
-            auto _loop  = 4;
+            auto _err   = false;
+            auto _loop  = 8;
             auto _bytes = 0UL;
 
             rx_fifo.Reset();
@@ -181,18 +192,22 @@ int main(int argc, char* argv[]) {
             GProfile profile;
             profile.Start();
             // -------------------------------------------------------------------
-            for (decltype(_loop) i{0}; (i < _loop) && _res; ++i) {
-                _res = rx_fifo.WaitThenClearEvent();
-                if (_res) {
-                    auto level{rx_fifo.GetRxLengthLevel(&_err)};
+            for (decltype(_loop) i{0}; (i < _loop) && !_err; ++i) {
+                _err = !rx_fifo.WaitThenClearEvent();
+                if (!_err) {
+                    auto level{rx_fifo.GetRxLengthLevel(_err)};
                     if (!_err && level > 0) {
-                        auto words{rx_fifo.GetRxPacketWords(&_err)};
+                        auto words{rx_fifo.GetRxPacketWords(_err)};
                         if (!_err && words > 7) {
-                            _res = rx_fifo.ReadPacket(rx_buff.ptr, words);
-                            if (_res) {
-                                dump_packet_words(rx_buff.ptr, words);
-                                _bytes += FIFO_WORD_SIZE * words;
+                            auto dst_buf{rx_buff.Writing_Start(_err)};
+                            if (!_err) {
+                                _err = !rx_fifo.ReadPacket(dst_buf->data(), words);
+                                if (!_err) {
+                                    _err = !dst_buf->used(words);
+                                    _bytes += FIFO_WORD_SIZE * words;
+                                }
                             }
+                            rx_buff.Writing_Stop(_err);
                         }
                     }
                 }
@@ -200,6 +215,14 @@ int main(int argc, char* argv[]) {
             // -------------------------------------------------------------------
             profile.Stop();
             LOG_FORMAT(info, "[RX] Data speed %0.3f Mbps", double(8 * _bytes) / profile.us());
+
+            for (decltype(_loop) i{0}; (i < _loop) && !_err; ++i) {
+                auto src_buf{rx_buff.Reading_Start(_err)};
+                if (!_err) {
+                    dump_packet_words(src_buf->data(), src_buf->used());
+                }
+                rx_buff.Reading_Stop(_err);
+            }
 
             rx_fifo.Close();
         }
