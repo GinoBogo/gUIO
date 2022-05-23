2cac9d38326cc759d0daa69a98a2647558bd69a2
Gino Bogo <gino.bogo@gmail.com>
2022-05-23 09:55:01 +0200

    Added new macros in the "GDefine.hpp" file.

--- lib/GDefine.hpp
+++ lib/GDefine.hpp
@@ -14,47 +14,78 @@
 #define BREAK_IF(_condition) \
     if (_condition) break
 
-#define BREAK_AND_LOG_IF(_condition, _type, _format, ...) \
-    if (_condition) {                                     \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);          \
-        break;                                            \
+#define BREAK_IF_THEN_LOG(_condition, _type, _format, ...) \
+    if (_condition) {                                      \
+        LOG_FORMAT(_type, _format, __VA_ARGS__);           \
+        break;                                             \
     }
+
+#define BREAK_IF_ELSE_LOG(_condition, _type, _format, ...) \
+    if (_condition) {                                      \
+        break;                                             \
+    }                                                      \
+    LOG_FORMAT(_type, _format, __VA_ARGS__);
 
 #define CONTINUE_IF(_condition) \
     if (_condition) continue
 
-#define CONTINUE_AND_LOG_IF(_condition, _type, _format, ...) \
-    if (_condition) {                                        \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);             \
-        continue;                                            \
+#define CONTINUE_IF_THEN_LOG(_condition, _type, _format, ...) \
+    if (_condition) {                                         \
+        LOG_FORMAT(_type, _format, __VA_ARGS__);              \
+        continue;                                             \
     }
+
+#define CONTINUE_IF_ELSE_LOG(_condition, _type, _format, ...) \
+    if (_condition) {                                         \
+        continue;                                             \
+    }                                                         \
+    LOG_FORMAT(_type, _format, __VA_ARGS__);
 
 #define EXIT_IF(_condition, _status) \
     if (_condition) exit(_status)
 
-#define EXIT_AND_LOG_IF(_condition, _status, _type, _format, ...) \
-    if (_condition) {                                             \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);                  \
-        exit(_status);                                            \
+#define EXIT_IF_THEN_LOG(_condition, _status, _type, _format, ...) \
+    if (_condition) {                                              \
+        LOG_FORMAT(_type, _format, __VA_ARGS__);                   \
+        exit(_status);                                             \
     }
+
+#define EXIT_IF_ELSE_LOG(_condition, _status, _type, _format, ...) \
+    if (_condition) {                                              \
+        exit(_status);                                             \
+    }                                                              \
+    LOG_FORMAT(_type, _format, __VA_ARGS__);
 
 #define GOTO_IF(_condition, _label) \
     if (_condition) goto _label
 
-#define GOTO_AND_LOG_IF(_condition, _label, _type, _format, ...) \
-    if (_condition) {                                            \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);                 \
-        goto _label;                                             \
+#define GOTO_IF_THEN_LOG(_condition, _label, _type, _format, ...) \
+    if (_condition) {                                             \
+        LOG_FORMAT(_type, _format, __VA_ARGS__);                  \
+        goto _label;                                              \
     }
+
+#define GOTO_IF_ELSE_LOG(_condition, _label, _type, _format, ...) \
+    if (_condition) {                                             \
+        LOG_FORMAT(_type, _format, __VA_ARGS__);                  \
+        goto _label;                                              \
+    }                                                             \
+    LOG_FORMAT(_type, _format, __VA_ARGS__);
 
 #define RETURN_IF(_condition) \
     if (_condition) return
 
-#define RETURN_AND_LOG_IF(_condition, _type, _format, ...) \
-    if (_condition) {                                      \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);           \
-        return;                                            \
+#define RETURN_IF_THEN_LOG(_condition, _type, _format, ...) \
+    if (_condition) {                                       \
+        LOG_FORMAT(_type, _format, __VA_ARGS__);            \
+        return;                                             \
     }
+
+#define RETURN_IF_ELSE_LOG(_condition, _type, _format, ...) \
+    if (_condition) {                                       \
+        return;                                             \
+    }                                                       \
+    LOG_FORMAT(_type, _format, __VA_ARGS__);
 
 // SECTION: general purpose
 
