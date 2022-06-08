ca4b7bda6f40ba80eff82275989fce35525f8632
Gino Bogo <gino.bogo@gmail.com>
2022-06-08 14:32:35 +0200

    Added the "IsLevelChanged" function to "GArrayRoller" class.

--- lib/GArrayRoller.hpp
+++ lib/GArrayRoller.hpp
@@ -13,24 +13,36 @@
 #include "GArray.hpp"
 #include "GLogger.hpp"
 
+#include <algorithm>   // std::min
 #include <mutex>       // mutex, lock_guard
 #include <type_traits> // is_fundamental_v
 
 template <typename T> class GArrayRoller {
     public:
     typedef enum {
-        IS_UNCLAIMED, // Finite-State Machine
+        IS_UNCLAIMED,
         IS_READING,
         IS_WRITING,
         IS_READING_AND_WRITING
-    } fsm_state_t;
 
-    GArrayRoller(size_t array_length, size_t arrays_number, const std::string& tag_name = "") {
+    } fsm_states_t;
+
+    typedef enum {
+        TRANSITION_OFF,
+        REGULAR_LEVEL,
+        MAX_LEVEL_PASSED,
+        MIN_LEVEL_PASSED
+
+    } fsm_levels_t;
+
+    GArrayRoller(size_t array_length, size_t arrays_number, const std::string& tag_name = "", int max_level = -1, int min_level = -1) {
         static_assert(std::is_fundamental_v<T>, "Type not supported.");
 
-        m_length   = array_length;
-        m_number   = arrays_number;
-        m_tag_name = tag_name.empty() ? "Array Roller" : "\"" + tag_name + "\" Array Roller";
+        m_length    = array_length;
+        m_number    = arrays_number;
+        m_tag_name  = tag_name.empty() ? "Array Roller" : "\"" + tag_name + "\" Array Roller";
+        m_max_level = max_level < 1 ? -1 : std::min(max_level, static_cast<int>(m_number));
+        m_min_level = min_level < 0 ? -1 : std::min(min_level, static_cast<int>(m_number));
 
         if (m_length > 0 && m_number > 0) {
             m_arrays = new GArray<T>*[m_number];
@@ -39,7 +51,7 @@ template <typename T> class GArrayRoller {
             }
         }
         Reset();
-        LOG_FORMAT(debug, "%s constructor [%lu, %lu]", m_tag_name.c_str(), m_length, m_number);
+        LOG_FORMAT(debug, "%s constructor [%lu, %lu, %d, %d]", m_tag_name.c_str(), m_length, m_number, m_max_level, m_min_level);
     }
 
     GArrayRoller(const GArrayRoller& array_roller) {
@@ -55,21 +67,25 @@ template <typename T> class GArrayRoller {
         if (this != &array_roller) {
             release_resources();
 
-            m_length = array_roller.m_length;
-            m_number = array_roller.m_number;
+            m_length    = array_roller.m_length;
+            m_number    = array_roller.m_number;
+            m_tag_name  = array_roller.m_tag_name;
+            m_max_level = array_roller.m_max_level;
+            m_min_level = array_roller.m_min_level;
 
             if (m_length > 0 && m_number > 0) {
                 m_arrays = new GArray<T>*[m_number];
                 for (decltype(m_number) i{0}; i < m_number; ++i) {
                     m_arrays[i] = new GArray<T>(*array_roller.m_arrays[i]);
                 }
-                m_status = array_roller.m_status;
-                m_errors = array_roller.m_errors;
-                m_used   = array_roller.m_used;
-                m_iR     = array_roller.m_iR;
-                m_iW     = array_roller.m_iW;
+                m_fsm_state = array_roller.m_fsm_state;
+                m_fsm_level = array_roller.m_fsm_level;
+                m_errors    = array_roller.m_errors;
+                m_used      = array_roller.m_used;
+                m_iR        = array_roller.m_iR;
+                m_iW        = array_roller.m_iW;
             }
-            LOG_FORMAT(debug, "%s constructor [%lu, %lu]", m_tag_name.c_str(), m_length, m_number);
+            LOG_FORMAT(debug, "%s constructor [%lu, %lu, %d, %d]", m_tag_name.c_str(), m_length, m_number, m_max_level, m_min_level);
         }
         return *this;
     }
@@ -83,11 +99,19 @@ template <typename T> class GArrayRoller {
             }
         }
 
-        m_status = IS_UNCLAIMED;
-        m_errors = 0;
-        m_used   = 0;
-        m_iR     = 0;
-        m_iW     = 0;
+        m_fsm_state = IS_UNCLAIMED;
+        m_fsm_level = TRANSITION_OFF;
+        m_errors    = 0;
+        m_used      = 0;
+        m_iR        = 0;
+        m_iW        = 0;
+
+        auto check_1{m_max_level == m_min_level};
+        auto check_2{m_max_level > 0 && m_max_level < m_min_level};
+
+        if (!check_1 && !check_2) {
+            m_fsm_level = MIN_LEVEL_PASSED;
+        }
     }
 
     auto Reading_Start(bool& error) {
@@ -96,9 +120,9 @@ template <typename T> class GArrayRoller {
         error = (m_used == 0);
 
         if (!error) {
-            switch (m_status) {
+            switch (m_fsm_state) {
                 case IS_UNCLAIMED:
-                    m_status = IS_READING;
+                    m_fsm_state = IS_READING;
                     break;
 
                 case IS_READING:
@@ -107,7 +131,7 @@ template <typename T> class GArrayRoller {
                     break;
 
                 case IS_WRITING:
-                    m_status = IS_READING_AND_WRITING;
+                    m_fsm_state = IS_READING_AND_WRITING;
                     break;
 
                 default:
@@ -126,7 +150,7 @@ template <typename T> class GArrayRoller {
 
         error = false;
 
-        switch (m_status) {
+        switch (m_fsm_state) {
             case IS_UNCLAIMED:
             case IS_WRITING:
                 error = true;
@@ -137,7 +161,7 @@ template <typename T> class GArrayRoller {
                     m_iR = 0;
                 }
                 m_used--;
-                m_status = IS_UNCLAIMED;
+                m_fsm_state = IS_UNCLAIMED;
                 break;
 
             case IS_READING_AND_WRITING:
@@ -145,7 +169,7 @@ template <typename T> class GArrayRoller {
                     m_iR = 0;
                 }
                 m_used--;
-                m_status = IS_WRITING;
+                m_fsm_state = IS_WRITING;
                 break;
 
             default:
@@ -162,9 +186,9 @@ template <typename T> class GArrayRoller {
         error = !(m_used < m_number);
 
         if (!error) {
-            switch (m_status) {
+            switch (m_fsm_state) {
                 case IS_UNCLAIMED:
-                    m_status = IS_WRITING;
+                    m_fsm_state = IS_WRITING;
                     break;
 
                 case IS_WRITING:
@@ -173,7 +197,7 @@ template <typename T> class GArrayRoller {
                     break;
 
                 case IS_READING:
-                    m_status = IS_READING_AND_WRITING;
+                    m_fsm_state = IS_READING_AND_WRITING;
                     break;
 
                 default:
@@ -192,7 +216,7 @@ template <typename T> class GArrayRoller {
 
         error = false;
 
-        switch (m_status) {
+        switch (m_fsm_state) {
             case IS_UNCLAIMED:
             case IS_READING:
                 error = true;
@@ -203,7 +227,7 @@ template <typename T> class GArrayRoller {
                     m_iW = 0;
                 }
                 m_used++;
-                m_status = IS_UNCLAIMED;
+                m_fsm_state = IS_UNCLAIMED;
                 break;
 
             case IS_READING_AND_WRITING:
@@ -211,7 +235,7 @@ template <typename T> class GArrayRoller {
                     m_iW = 0;
                 }
                 m_used++;
-                m_status = IS_READING;
+                m_fsm_state = IS_READING;
                 break;
 
             default:
@@ -222,6 +246,36 @@ template <typename T> class GArrayRoller {
         m_errors += (unsigned)error;
     }
 
+    bool IsLevelChanged(fsm_levels_t* new_level = nullptr, fsm_levels_t* old_level = nullptr) {
+        std::lock_guard<std::mutex> _lock(m_mutex);
+
+        auto _state_changed{false};
+
+        if (old_level != nullptr) {
+            *old_level = m_fsm_level;
+        }
+
+        if (m_fsm_level != TRANSITION_OFF) {
+            auto _current_level{static_cast<int>(m_used)};
+
+            if (m_max_level > 0 && _current_level >= m_max_level) {
+                _state_changed = m_fsm_level != MAX_LEVEL_PASSED;
+                m_fsm_level    = MAX_LEVEL_PASSED;
+            }
+
+            if (m_min_level > 0 && _current_level <= m_min_level) {
+                _state_changed = m_fsm_level != MIN_LEVEL_PASSED;
+                m_fsm_level    = MIN_LEVEL_PASSED;
+            }
+        }
+
+        if (new_level != nullptr) {
+            *new_level = m_fsm_level;
+        }
+
+        return _state_changed;
+    }
+
     [[nodiscard]] auto length() const {
         return m_length;
     }
@@ -230,9 +284,22 @@ template <typename T> class GArrayRoller {
         return m_number;
     }
 
+    [[nodiscard]] auto max_level() const {
+        return m_max_level;
+    }
+
+    [[nodiscard]] auto min_level() const {
+        return m_min_level;
+    }
+
     // WARNING: thread unsafe
-    [[nodiscard]] auto status() const {
-        return m_status;
+    [[nodiscard]] auto fsm_state() const {
+        return m_fsm_state;
+    }
+
+    // WARNING: thread unsafe
+    [[nodiscard]] auto fsm_level() const {
+        return m_fsm_level;
     }
 
     // WARNING: thread unsafe
@@ -265,9 +332,13 @@ template <typename T> class GArrayRoller {
 
     size_t      m_length;
     size_t      m_number;
-    fsm_state_t m_status;
-    size_t      m_errors;
     std::string m_tag_name;
+    int         m_max_level;
+    int         m_min_level;
+
+    fsm_states_t m_fsm_state;
+    fsm_levels_t m_fsm_level;
+    size_t       m_errors;
 
     GArray<T>** m_arrays{nullptr};
     size_t      m_used;
