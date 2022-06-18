d45bba2613a76759c591e657085ebd747a1c088c
Gino Bogo <gino.bogo@gmail.com>
2022-06-18 17:30:58 +0200

    Major update of "GDefine" macros.

--- lib/GDefine.hpp
+++ lib/GDefine.hpp
@@ -14,96 +14,36 @@
 
 // SECTION: control flow statements
 
-#define LOG_IF(_condition, _type, _format, ...)  \
-    if (_condition) {                            \
-        LOG_FORMAT(_type, _format, __VA_ARGS__); \
-    }
-
-#define BREAK_IF(_condition, ...) \
-    __VA_ARGS__;                  \
-    if (_condition) break
-
-#define BREAK_IF_THEN_LOG(_condition, _type, _format, ...) \
-    if (_condition) {                                      \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);           \
-        break;                                             \
-    }
-
-#define BREAK_IF_ELSE_LOG(_condition, _type, _format, ...) \
-    if (_condition) {                                      \
-        break;                                             \
-    }                                                      \
-    LOG_FORMAT(_type, _format, __VA_ARGS__);
-
-#define CONTINUE_IF(_condition, ...) \
-    __VA_ARGS__;                     \
-    if (_condition) continue
-
-#define CONTINUE_IF_THEN_LOG(_condition, _type, _format, ...) \
-    if (_condition) {                                         \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);              \
-        continue;                                             \
-    }
-
-#define CONTINUE_IF_ELSE_LOG(_condition, _type, _format, ...) \
-    if (_condition) {                                         \
-        continue;                                             \
-    }                                                         \
-    LOG_FORMAT(_type, _format, __VA_ARGS__);
-
-#define EXIT_IF(_condition, _status, ...) \
-    __VA_ARGS__;                          \
-    if (_condition) exit(_status)
-
-#define EXIT_IF_THEN_LOG(_condition, _status, _type, _format, ...) \
-    if (_condition) {                                              \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);                   \
-        exit(_status);                                             \
-    }
-
-#define EXIT_IF_ELSE_LOG(_condition, _status, _type, _format, ...) \
-    if (_condition) {                                              \
-        exit(_status);                                             \
-    }                                                              \
-    LOG_FORMAT(_type, _format, __VA_ARGS__);
-
-#define GOTO_IF(_condition, _label, ...) \
-    __VA_ARGS__;                         \
-    if (_condition) goto _label
-
-#define GOTO_IF_THEN(_condition, _label, ...) \
-    if (_condition) {                         \
-        __VA_ARGS__;                          \
-        goto _label;                          \
-    }
-
-#define GOTO_IF_THEN_LOG(_condition, _label, _type, _format, ...) \
-    if (_condition) {                                             \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);                  \
-        goto _label;                                              \
-    }
-
-#define GOTO_IF_ELSE_LOG(_condition, _label, _type, _format, ...) \
-    if (_condition) {                                             \
-        goto _label;                                              \
-    }                                                             \
-    LOG_FORMAT(_type, _format, __VA_ARGS__);
-
-#define RETURN_IF(_condition, ...) \
-    __VA_ARGS__;                   \
-    if (_condition) return
-
-#define RETURN_IF_THEN_LOG(_condition, _type, _format, ...) \
-    if (_condition) {                                       \
-        LOG_FORMAT(_type, _format, __VA_ARGS__);            \
-        return;                                             \
-    }
-
-#define RETURN_IF_ELSE_LOG(_condition, _type, _format, ...) \
-    if (_condition) {                                       \
-        return;                                             \
-    }                                                       \
-    LOG_FORMAT(_type, _format, __VA_ARGS__);
+// clang-format off
+#define BREAK_IF_AND_LOG(_condition, _type, _format, ...)         /**/ if (_condition) { LOG_FORMAT(_type, _format, __VA_ARGS__); break; }
+#define BREAK_IF_AND(_condition, ...)                             /**/ if (_condition) { __VA_ARGS__; break; }
+#define BREAK_IF_OR_LOG(_condition, _type, _format, ...)          /**/ if (_condition) break; LOG_FORMAT(_type, _format, __VA_ARGS__)
+#define BREAK_IF_OR(_condition, ...)                              /**/ if (_condition) break; __VA_ARGS__
+#define BREAK_IF(_condition, ...)                                 /**/ __VA_ARGS__; if (_condition) break
+#define CONTINUE_IF_AND_LOG(_condition, _type, _format, ...)      /**/ if (_condition) { LOG_FORMAT(_type, _format, __VA_ARGS__); continue; }
+#define CONTINUE_IF_AND(_condition, ...)                          /**/ if (_condition) { __VA_ARGS__; continue; }
+#define CONTINUE_IF_OR_LOG(_condition, _type, _format, ...)       /**/ if (_condition) continue; LOG_FORMAT(_type, _format, __VA_ARGS__)
+#define CONTINUE_IF_OR(_condition, ...)                           /**/ if (_condition) continue; __VA_ARGS__
+#define CONTINUE_IF(_condition, ...)                              /**/ __VA_ARGS__; if (_condition) continue
+#define DO(...)                                                   /**/ __VA_ARGS__
+#define EXIT_IF_AND_LOG(_condition, _status, _type, _format, ...) /**/ if (_condition) { LOG_FORMAT(_type, _format, __VA_ARGS__); exit(_status); }
+#define EXIT_IF_AND(_condition, _status, ...)                     /**/ if (_condition) { __VA_ARGS__; exit(_status); }
+#define EXIT_IF_OR_LOG(_condition, _status, _type, _format, ...)  /**/ if (_condition) exit(_status); LOG_FORMAT(_type, _format, __VA_ARGS__)
+#define EXIT_IF_OR(_condition, _status, ...)                      /**/ if (_condition) exit(_status); __VA_ARGS__
+#define EXIT_IF(_condition, _status, ...)                         /**/ __VA_ARGS__; if (_condition) exit(_status)
+#define GOTO_IF_AND_LOG(_condition, _label, _type, _format, ...)  /**/ if (_condition) { LOG_FORMAT(_type, _format, __VA_ARGS__); goto _label; }
+#define GOTO_IF_AND(_condition, _label, ...)                      /**/ if (_condition) { __VA_ARGS__; goto _label; }
+#define GOTO_IF_OR_LOG(_condition, _label, _type, _format, ...)   /**/ if (_condition) goto _label; LOG_FORMAT(_type, _format, __VA_ARGS__)
+#define GOTO_IF_OR(_condition, _label, ...)                       /**/ if (_condition) goto _label; __VA_ARGS__
+#define GOTO_IF(_condition, _label, ...)                          /**/ __VA_ARGS__; if (_condition) goto _label
+#define IF(_condition, ...)                                       /**/ _condition; if (_condition) __VA_ARGS__
+#define LOG_IF(_condition, _type, _format, ...)                   /**/ if (_condition) LOG_FORMAT(_type, _format, __VA_ARGS__)
+#define RETURN_IF_AND_LOG(_condition, _type, _format, ...)        /**/ if (_condition) { LOG_FORMAT(_type, _format, __VA_ARGS__); return; }
+#define RETURN_IF_AND(_condition, ...)                            /**/ if (_condition) { __VA_ARGS__; return; }
+#define RETURN_IF_OR_LOG(_condition, _type, _format, ...)         /**/ if (_condition) return; LOG_FORMAT(_type, _format, __VA_ARGS__)
+#define RETURN_IF_OR(_condition, ...)                             /**/ if (_condition) return; __VA_ARGS__
+#define RETURN_IF(_condition, ...)                                /**/ __VA_ARGS__; if (_condition) return
+// clang-format on
 
 // SECTION: general purpose
 
