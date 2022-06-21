48955827dcd27eaeaf56687a3b68b6f6068e99c7
Gino Bogo <gino.bogo@gmail.com>
2022-06-21 09:24:19 +0200

    Code regression for "IRQ_Clear" function.

--- uio/GUIOdevice.hpp
+++ uio/GUIOdevice.hpp
@@ -63,7 +63,7 @@ class GUIOdevice {
         return false;
     }
 
-    [[nodiscard]] bool IRQ_Clear() const {
+    bool IRQ_Clear() const {
         int32_t _val{0x00000001};
 
         auto _ret{write(m_dev.fd, &_val, sizeof(_val))};
